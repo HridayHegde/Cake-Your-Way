48d7be17d0ebddfca1e09b7e00e2bf309c1692fd
Hriday Hegde <hridayhegde1999@gmail.com>
2019-10-23 00:47:22 +0530

    Paytm Gateway Commit

--- assets/PayTmKit/PaytmKit/pgRedirect.php
+++ assets/PayTmKit/PaytmKit/pgRedirect.php
@@ -25,7 +25,7 @@ $paramList["TXN_AMOUNT"] = $TXN_AMOUNT;
 $paramList["WEBSITE"] = PAYTM_MERCHANT_WEBSITE;
 
 
-$paramList["CALLBACK_URL"] = "assets/PayTmKit/PaytmKit/pgResponse.php";
+$paramList["CALLBACK_URL"] = "http://cakeyourway.gearhostpreview.com/assets/PayTmKit/PaytmKit/pgResponse.php";
 /*
 $paramList["MSISDN"] = $MSISDN; //Mobile number of customer
 $paramList["EMAIL"] = $EMAIL; //Email ID of customer
